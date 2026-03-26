40f5ba9d5788ae9110cd61b2b6c2cdb60d0980a7
joaopedro3434 <joaoanjos.39@gmail.com>
2026-03-26 20:09:04 -0300

    Add files via upload

---
+++ EX3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int D;
+    int a;
+    
+    cout << "informe a area apatir da diagonal: ";
+    cin >> D;
+    
+    a = 
+    
+    cout << "A area do quadrado calculada APARTIR DA DIAGONAL: " << a;
+    
+    return 0;
---
+++ ex2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    int L;
+    int a;
+    
+    cout << "informe a base do quadrado: ";
+    cin >> L;
+    
+    a = L * L;
+    
+    cout << "A area do quadrado calculada: " << a;
+    
+    return 0;
+}
